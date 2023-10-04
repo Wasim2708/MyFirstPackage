e9cd3de2f15e399ab50c720bf8b0fab33d5c6c74
wasim-18972 <wasim-18972@zohocorp.com>
2023-10-04 09:16:58 +0530

    added new method in my package

---
+++ Sources/MyFirstPackage/MyMethods.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by wasim-18972 on 04/10/23.
+//
+
+import Foundation
+
+
+public class MyMethods {
+    
+    func addTwoNumbers(a: Int, b: Int) -> Int {  a + b }
+    
+}
