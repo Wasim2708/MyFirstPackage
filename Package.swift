3fe5a900c1264c5324bfbb9789fae7505f3d493c
wasim-18972 <wasim-18972@zohocorp.com>
2023-10-04 11:22:22 +0530

    did correction

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ZohoDeskPortalAPIKit",
-            url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/3.0.0-beta-12/ZohoDeskPortalAPIKit.zip",
+            url: "https://github.com/Wasim2708/MyFirstPackage.git/ZohoDeskPortalAPIKit.zip",
             checksum: "e792f46a4a825ebd3afa530637282f08ddacf2e61e7d530664fd717ea1b191dc"
         )
     ]
