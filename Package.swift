b127f531b7adf4361249bbb8f391c5c50fbc9a7f
wasim-18972 <wasim-18972@zohocorp.com>
2023-10-04 13:52:58 +0530

    did correction

--- Package.swift
+++ Package.swift
@@ -12,22 +12,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MyFirstPackage",
-            targets: ["MyFirstPackage"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0")
+            targets: ["ZohoDeskPortalAPIKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyFirstPackage",
-            dependencies: ["Alamofire"]
+        .binaryTarget(
+            name: "ZohoDeskPortalAPIKit",
+            url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/3.0.0-beta-12/ZohoDeskPortalAPIKit.zip",
+            checksum: "e792f46a4a825ebd3afa530637282f08ddacf2e61e7d530664fd717ea1b191dc"
         )
-//        .binaryTarget(
-//            name: "ZohoDeskPortalAPIKit",
-//            url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/3.0.0-beta-12/ZohoDeskPortalAPIKit.zip",
-//            checksum: "e792f46a4a825ebd3afa530637282f08ddacf2e61e7d530664fd717ea1b191dc"
-//        )
     ]
 )
