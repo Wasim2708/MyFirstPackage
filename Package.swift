923764136c9272e50d956407fb7bae2b67cd94a8
wasim-18972 <wasim-18972@zohocorp.com>
2023-10-04 10:41:51 +0530

    Adding binary target in remote

--- Package.swift
+++ Package.swift
@@ -5,25 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "MyFirstPackage",
+    platforms: [
+        .iOS(.v11),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MyFirstPackage",
-            targets: ["MyFirstPackage"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0")
+            targets: ["MyFirstPackage", "ZohoDeskPortalAPIKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MyFirstPackage",
-            dependencies: ["Alamofire"]),
-        .testTarget(
-            name: "MyFirstPackageTests",
-            dependencies: ["MyFirstPackage"]),
+            name: "MyFirstPackage"
+        ),
+        .binaryTarget(
+            name: "ZohoDeskPortalAPIKit",
+            url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/3.0.0-beta-12/ZohoDeskPortalAPIKit.zip",
+            checksum: "e792f46a4a825ebd3afa530637282f08ddacf2e61e7d530664fd717ea1b191dc")
     ]
 )
